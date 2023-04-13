f5daf67bd439709cacd5dac208f97f59f79f3a96
ShunPeng1 <shunpeng1@gmail.com>
2023-04-13 16:12:12 +0700

    feat: lab 4 3

--- Lab4/3/queue.c
+++ Lab4/3/queue.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include "queue.h"
 #include <pthread.h>
+#include <stdio.h>
+
 
 /* Remember to initilize the queue before using it */
 void initialize_queue(struct pqueue_t * q) {
@@ -22,10 +24,13 @@ struct pcb_t * de_queue(struct pqueue_t * q) {
 	// and tail if necessary. Remember to use 'lock' to avoid race
 	// condition
 	
-	// YOUR CODE HERE
+
 	pthread_mutex_lock(&q->lock);
+	// YOUR CODE HERE
+	
+	//printf("dequeue begin\n");
 	if(q->head == NULL){
-		
+
 	}
 	else if(q->head == q->tail){
 		proc = q->head->data;
@@ -40,6 +45,8 @@ struct pcb_t * de_queue(struct pqueue_t * q) {
 		q->head = temp;
 	}
 
+
+	//printf("dequeue end\n");
 	pthread_mutex_unlock(&q->lock);
 
 	return proc;
@@ -51,7 +58,9 @@ void en_queue(struct pqueue_t * q, struct pcb_t * proc) {
 	// Remember to use 'lock' to avoid race condition
 	
 	// YOUR CODE HERE
+
 	pthread_mutex_lock(&q->lock);
+	//printf("enqueue begin\n");
 
 	struct qitem_t* item = malloc(sizeof(struct qitem_t));
 	item->next = NULL;
@@ -65,7 +74,73 @@ void en_queue(struct pqueue_t * q, struct pcb_t * proc) {
 		q->tail = item;
 	}
 
+	//("enqueue end\n");
 	pthread_mutex_unlock(&q->lock);
+}
+
+
+/* Return and delete a process with first highest prioirty in a queue */
+struct pcb_t* pick_out_first_highest_priority(struct pqueue_t * q){
+	if(q->head == NULL){
+		return NULL;
+	}
+
+	pthread_mutex_lock( &q->lock);
+	
+	//printf("Find %d->",  q->head->data->arrival_time);
+	// Find first highest priority
+	int highestPriority = q->head->data->priority;
+	struct qitem_t* highestPriorityItem = q->head, *currentItem = q->head, *lastHighestPriorityItem = NULL;
+	while(currentItem->next != NULL){
+		struct qitem_t * next = currentItem->next;
+
+		if(next->data->priority > highestPriority){
+			highestPriority = next->data->priority;
+			lastHighestPriorityItem = currentItem;
+			highestPriorityItem = next;
+		}
+		//printf("%d->",currentItem->next->data->arrival_time);
+		currentItem = next;
+	}
+
+	struct pcb_t *proc = NULL;
+	
+	//printf("\nRemove arrival time: %d, burst time: %d, priority: %d\n", highestPriorityItem->data->arrival_time, highestPriorityItem->data->burst_time, highestPriorityItem->data->priority);
+	
+	// Remove first highest priority 
+	if(q->head == q->tail){
+		proc = q->head->data;
+		
+		free(q->head);
+		q->head = q->tail = NULL;
+	}
+	else if(q->head == highestPriorityItem){
+		proc = q->head->data;
+
+		struct qitem_t * temp = q->head->next; 
+		free(q->head);
+		q->head = temp;
+	}
+	else if(q->tail == highestPriorityItem){
+		proc = q->tail->data;
+
+		//printf("Last %d\n", lastHighestPriorityItem->data->arrival_time);
+		free(q->tail);
+		q->tail = lastHighestPriorityItem;
+		q->tail->next = NULL;
+	}
+	else{
+		proc = highestPriorityItem->data;
+
+		//printf("Last %d\n", lastHighestPriorityItem->data->arrival_time);
+		lastHighestPriorityItem->next = highestPriorityItem->next;
+		free(highestPriorityItem);
+	}
+
+
+	pthread_mutex_unlock(&q->lock);
+
+	return proc;
 }
 
 
